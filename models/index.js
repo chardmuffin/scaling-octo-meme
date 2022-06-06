217741b07ffad8b04bc91b790f937354b24712b8
Richard Huffman <richardhuffman96@gmail.com>
2022-06-06 04:50:21 -0400

    added models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreign_key: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreign_key: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreign_key: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreign_key: 'tag_id'
+})
 
 module.exports = {
   Product,
